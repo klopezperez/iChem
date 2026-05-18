9a4b80d6bd2c3b327886335c0f6fdc4bc60d7734
klopezperez <klopezperez@ufl.edu>
2026-05-18 17:35:03 -0400

    utils restructure

--- iChem/utils/__init__.py
+++ iChem/utils/__init__.py
@@ -1,2 +1,17 @@
 from .fingerprints import binary_fps, real_fps, count_fps
-from .utils import load_multiple_smiles, load_smiles, load_smiles_gzipped
+
+# Export common utility functions from utils module
+from .utils import (
+	load_multiple_smiles,
+	load_smiles,
+	load_smiles_gzipped,
+	load_smiles_and_ids,
+	minmax_norm,
+	normalize_fps,
+	npy_to_rdkit,
+	rdkit_pairwise_sim,
+	rdkit_pairwise_matrix,
+	pairwise_average,
+	pairwise_average_real,
+	smiles_standarization,
+)
